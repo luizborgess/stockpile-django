6d42f85239f4a825f92f17f673d3614d3d8357ff
Luiz <luiz.borgess@hotmail.com>
2025-12-03 16:45:08 -0300

    fix db google

--- stockpile_django/stockpile_django/settings.py
+++ stockpile_django/stockpile_django/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -74,11 +74,12 @@ WSGI_APPLICATION = 'stockpile_django.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+DB_PATH = os.getenv("SQLITE_PATH", BASE_DIR / "db.sqlite3")
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': DB_PATH,
     }
 }
 
